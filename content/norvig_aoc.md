7eeabf267f2122a26f70f9da4f9c5fe431ae4901
Viet Hung Nguyen <hvn@familug.org>
2021-01-09 22:20:41 +0700

    Add

---
+++ content/norvig_aoc.md
@@ -0,0 +1,20 @@
+title: Code Advent of code 2020 của giám đốc nghiên cứu Google - Peter Norvig
+date: 2021-01-09
+modified: 2021-01-09
+tags: văn mẫu, beautifulcode, aoc, adventofcode
+category: news
+slug: aoc2020-norvig
+authors: Pymier0
+description: Lý do bạn quá bận để tham gia "adventofcode 2020"? Peter Norvig - giám đốc nghiên cứu của Google đã xử đẹp 47/50 bài với 1 file jupyter notebook.
+
+Lý do bạn quá bận để tham gia "adventofcode 2020"?
+
+Hãy nghĩ lại, khi giám đốc nghiên cứu của Google - Peter Norvig đã xử đẹp 47/50 bài với 1 file jupyter notebook, với code siêu đẹp, siêu ngắn gọn. Và như thường lệ, đây là "văn mẫu" của các pythonista 😍
+
+[https://github.com/norvig/pytudes/blob/master/ipynb/Advent-2020.ipynb](https://github.com/norvig/pytudes/blob/master/ipynb/Advent-2020.ipynb)
+
+Đăng ký ngay tại [PyMI.vn](https://pymi.vn) để trở thành lập trình viên #python chuyên nghiệp.
+
+Lớp HCM khai giảng tối thứ 3 ngày 12/1/2021.
+
+#pymi #pymivn #pyfml #adventofcode
---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Pymiers'
+SITENAME = 'Tin tức Python PyMI.vn'
+SITEURL = ''
+FIRST_NAME = 'Pymiers'
+SITEDESCRIPTION = 'Tin tức Python, lớp học Python của Pymi.vn'
+GITHUB = 'https://github.com/pymivn'
+
+THEME = 'themes/brutalist'
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Ho_Chi_Minh'
+
+DEFAULT_LANG = 'vi'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Học Python', 'https://pymi.vn'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('PyFML', 'https://www.facebook.com/pyfml/'),)
+
+DEFAULT_PAGINATION = 10
+TYPOGRIFY = True
+
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
